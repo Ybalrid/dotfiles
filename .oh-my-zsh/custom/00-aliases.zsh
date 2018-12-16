197843c6e771dca30d6ea856a41263f25e0e64e1
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2018-12-16 16:04:55 +0000

    Added oh-my-zsh "custom" directory

---
+++ .oh-my-zsh/custom/00-aliases.zsh
@@ -0,0 +1,4 @@
+alias vim=nvim
+alias iTunes="dex ~/Desktop/iTunes.desktop"
+alias sl="sl -Flac"
+alias save_sreenshots="mv ~/*.png Pictures/Screenshots"
---
+++ .oh-my-zsh/custom/99-zsh_history_search_bindkey.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+
